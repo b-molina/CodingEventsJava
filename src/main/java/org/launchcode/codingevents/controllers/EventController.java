069c20392ea5be5b480da0b4aa19c41a0fd2f450
Bryanna Molina <bryannamolina@Bryannas-MBP.lan>
2024-02-12 15:00:41 -0500

    Studio

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
